9058821987b10447191b8e199b21d180da89a637
wickylove <39910654+wickylove@users.noreply.github.com>
2018-12-28 17:29:11 +0800

    Add files via upload

---
+++ vueT/vue15/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/alex">Alext</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+/* .router-link-active{
+  color: orange ;
+} */ 路由經過就會產生,可以在element中觀察
+.router-link-exact-active {
+  color : red ;
+}
+</style>
